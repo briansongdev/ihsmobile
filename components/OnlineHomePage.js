153f75520805169225a8144b39d4f517b5d18013
Brian S <brians3476@gmail.com>
2022-10-15 19:01:48 -0700

    Revised v2.2

--- components/OnlineHomePage.js
+++ components/OnlineHomePage.js
@@ -1189,10 +1189,8 @@ function HomeScreen({ navigation }) {
                       "",
                       "2:15 PM - 3:40 PM",
                     ];
-                    // const hr = new Date().getHours(),
-                    //   min = new Date().getMinutes();
-                    const hr = 4,
-                      min = 5;
+                    const hr = new Date().getHours(),
+                      min = new Date().getMinutes();
                     const chokepoints = [
                       [10, 25],
                       [0, 0],
